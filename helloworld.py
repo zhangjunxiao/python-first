609eb04108cb476effbe002743f3749e09bb81b6
zhangjunxiao <291722629@qq.com>
2016-11-16 09:05:10 +0800

    V0.0.0.1

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+print('hello world')
+print('hello world'+\
+        'zzzz')
+print("\a")
+# raw_input("\n\nPress the enter key to exit.")
+# eval("print('hello world')")
+print("Hello World")
+
+print " 张俊啸"
