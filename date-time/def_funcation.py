b01fcbca8e6a8391808a2cb5573078c59e1f3a3e
zhangjunxiao <291722629@qq.com>
2016-11-16 16:12:15 +0800

    V0.0.0.2

---
+++ date-time/def_funcation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding=utf-8
+import math
+def printme(str):
+    """打印任何传入的字符串"""
+    print str
+    return
+
+
+# 调用函数
+printme("我要调用用户自定义函数!");
+printme("再次调用同一函数");
+
+print dir(math)
