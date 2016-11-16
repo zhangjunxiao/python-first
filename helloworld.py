6dc36195322cf2f634510c0b7f8f957987d29809
zhangjunxiao <291722629@qq.com>
2016-11-16 10:00:57 +0800

    V0.0.0.2

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,14 @@
-#! /usr/bin/python
-print('hello world')
-print('hello world'+\
-        'zzzz')
-print("\a")
+#!/usr/bin/python
+# coding:utf-8
+
+import time
+print 'hello world'
+print 'hello world'+\
+        'zzzz'
+print "\a"
 # raw_input("\n\nPress the enter key to exit.")
 # eval("print('hello world')")
-print("Hello World")
+print "Hello World"
 
-print " 张俊啸"
+print "张俊啸"
+print time.time()
